b8b7f4edd57fa5124d4bd1f0ba6d7e3e11731ed4
Tin Goricki <exibodude@gmail.com>
2023-03-14 17:37:13 +0100

    Added homepage

--- src/home.js
+++ src/home.js
@@ -1,11 +1,22 @@
 function loadHomeInfo() {
     const pageContainer = document.querySelector(".page-container");
 
-    const restaurantName = document.createElement("div");
-    
-    restaurantName.textContent = "Papa John";
+    const restaurantInfoContainer = document.createElement("div");
 
-    pageContainer.appendChild(restaurantName);
+    restaurantInfoContainer.classList.add("restaurant-info");
+    
+    const restaurantName = document.createElement("p");
+
+    restaurantName.textContent = "Papa Johns";
+
+    const restaurantHours = document.createElement("p");
+
+    restaurantHours.textContent = "Mon-Fri: 7:00-20:00";
+
+    restaurantInfoContainer.appendChild(restaurantName);
+    restaurantInfoContainer.appendChild(restaurantHours)
+
+    pageContainer.appendChild(restaurantInfoContainer);
 }
 
 export default loadHomeInfo;
