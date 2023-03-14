c41f09da02766c62e7affe96e5753b0c610715d5
Tin Goricki <exibodude@gmail.com>
2023-03-14 16:37:13 +0100

    Added header and css for header

--- src/loadHomepage.js
+++ src/loadHomepage.js
@@ -3,15 +3,51 @@ import PizzaSlice from "./pizza_slice.jpg"
 function loadHomepage() {
     const container = document.querySelector("#content");
 
-    const restaurantName = document.createElement("div");
+    const header = document.createElement("div");
+    const homeBtn = document.createElement("div");
+    const menuBtn = document.createElement("div");
+    const contactBtn = document.createElement("div");
+
+    header.classList.add("header")
     
-    restaurantName.textContent = "Papa Johns pizzeria";
+    homeBtn.textContent = "Home";
+    menuBtn.textContent = "Menu";
+    contactBtn.textContent = "Contact";
 
-    const pizzaSlice = new Image();
-    pizzaSlice.src = PizzaSlice;
+    homeBtn.classList.add("btn");
+    menuBtn.classList.add("btn");
+    contactBtn.classList.add("btn");
+    
+    function addActiveClass ( element ) {
+        element.classList.add("active");
+    }
 
-    container.appendChild(restaurantName);
-    container.appendChild(pizzaSlice);
+    function removeActiveClass() {
+        homeBtn.classList.remove("active");
+        menuBtn.classList.remove("active");
+        contactBtn.classList.remove("active");
+    }
+
+    homeBtn.addEventListener("click", () => {
+        removeActiveClass();
+        addActiveClass(homeBtn);
+    });
+
+    menuBtn.addEventListener("click", () => {
+        removeActiveClass();
+        addActiveClass(menuBtn);
+    });
+
+    contactBtn.addEventListener("click", () => {
+        removeActiveClass();
+        addActiveClass(contactBtn);
+    });
+
+    header.appendChild(homeBtn);
+    header.appendChild(menuBtn);
+    header.appendChild(contactBtn);
+
+    container.appendChild(header);
 }
 
 export default loadHomepage;
